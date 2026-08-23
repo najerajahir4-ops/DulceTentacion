19f652d14356813ff980fa8508d384c2ca058fbe
Kenny Nájera <najerajahir4@gmail.com>
2026-08-23 03:03:04 -0500

    feat: replace vector doodles with realistic floating ingredients

--- app/page.tsx
+++ app/page.tsx
@@ -500,17 +500,6 @@ export default function LandingPage() {
       {/* HERO SECTION */}
       <section className="relative pt-32 pb-32 lg:pt-48 lg:pb-48 bg-background z-40 overflow-hidden">
         
-        {/* BACKGROUND DOODLES */}
-        <div className="absolute inset-0 pointer-events-none overflow-hidden opacity-[0.03] text-foreground">
-          <IceCream2 className="absolute top-[10%] left-[5%] w-24 h-24 -rotate-12" />
-          <Cherry className="absolute top-[20%] right-[10%] w-16 h-16 rotate-45" />
-          <Cookie className="absolute bottom-[30%] left-[15%] w-20 h-20 rotate-12" />
-          <Sparkles className="absolute top-[40%] left-[40%] w-12 h-12" />
-          <Star className="absolute bottom-[20%] right-[20%] w-16 h-16 -rotate-12" />
-          <Heart className="absolute top-[15%] left-[60%] w-14 h-14 rotate-12" />
-          <Leaf className="absolute bottom-[10%] left-[50%] w-20 h-20 -rotate-45" />
-        </div>
-
         {/* Animated Background Blobs (Melt System Aura) */}
         <div className="absolute inset-0 overflow-hidden pointer-events-none z-0">
           <motion.div 
@@ -596,14 +585,13 @@ export default function LandingPage() {
       {/* MENU / MELTING GALLERY */}
       <section className="pt-8 pb-12 bg-surface relative z-20 overflow-hidden">
         
-        {/* BACKGROUND DOODLES (MENU) */}
-        <div className="absolute inset-0 pointer-events-none overflow-hidden opacity-[0.03] text-foreground">
-          <IceCream2 className="absolute top-[5%] right-[5%] w-32 h-32 rotate-12" />
-          <Cookie className="absolute top-[30%] left-[8%] w-24 h-24 -rotate-12" />
-          <Cherry className="absolute bottom-[20%] right-[15%] w-20 h-20 -rotate-45" />
-          <Star className="absolute top-[15%] left-[20%] w-16 h-16 rotate-45" />
-          <Sparkles className="absolute bottom-[40%] left-[5%] w-12 h-12" />
-          <Heart className="absolute top-[40%] right-[30%] w-16 h-16 -rotate-12" />
+        {/* REALISTIC BACKGROUND ACCENTS (MENU) */}
+        <div className="absolute inset-0 pointer-events-none overflow-hidden z-0">
+          <img src="/images/strawberry_float.png" alt="" className="absolute top-[5%] right-[2%] w-32 h-32 md:w-48 md:h-48 object-contain rotate-12 blur-[2px] opacity-50" />
+          <img src="/images/chocolate_float.png" alt="" className="absolute top-[25%] left-[2%] w-24 h-24 md:w-36 md:h-36 object-contain -rotate-12 blur-[1px] opacity-60" />
+          <img src="/images/mint_float.png" alt="" className="absolute bottom-[15%] right-[8%] w-28 h-28 md:w-40 md:h-40 object-contain -rotate-45 blur-[3px] opacity-40" />
+          <img src="/images/strawberry_float.png" alt="" className="absolute top-[60%] left-[-2%] w-40 h-40 md:w-56 md:h-56 object-contain rotate-[60deg] blur-[5px] opacity-30" />
+          <img src="/images/chocolate_float.png" alt="" className="absolute bottom-[2%] left-[20%] w-20 h-20 md:w-28 md:h-28 object-contain rotate-90 blur-[2px] opacity-50" />
         </div>
 
         <div id="menu" className="max-w-7xl mx-auto px-6 mb-16 text-center scroll-mt-28 relative z-10">
